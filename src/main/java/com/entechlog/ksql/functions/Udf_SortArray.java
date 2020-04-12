9e1816cc5eba77faebd7a0476395607cb51bcd1d
Siva Nadesan <entechlog@gmail.com>
2020-04-12 18:06:40 -0400

    version : 0.1.04, Description: Created new UDF UDF_SORTARRAY

--- src/main/java/com/entechlog/ksql/functions/Udf_SortArray.java
+++ src/main/java/com/entechlog/ksql/functions/Udf_SortArray.java
@@ -21,15 +21,18 @@ public class Udf_SortArray {
         @UdfParameter(value = "sortOrder", description = "Sort Order") final String...sortOrder
     ) {
 
-//        List < String > outputArrayList = new ArrayList < > ();
-//        outputArrayList = inputArrayList;
+        List < String > outputArrayList = new ArrayList < > ();
+
+        for (int counter = 0; counter < inputArrayList.size(); counter++) {
+            outputArrayList.add(inputArrayList.get(counter));
+        }
 
         if (sortOrder.length > 0 && sortOrder[0].equals("DESC")) {
-            Collections.sort(inputArrayList, Collections.reverseOrder());
-            return inputArrayList;
+            Collections.sort(outputArrayList, Collections.reverseOrder());
+            return outputArrayList;
         } else {
-            Collections.sort(inputArrayList);
-            return inputArrayList;
+            Collections.sort(outputArrayList);
+            return outputArrayList;
         }
 
     }
